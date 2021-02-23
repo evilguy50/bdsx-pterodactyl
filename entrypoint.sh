a68999c7ba3152ceaef329c687dbaac81149eaf7
evilguy50 <robbryan42@gmail.com>
2021-02-22 18:14:50 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,7 @@
 #!/bin/ash
 
 # Make sure we're in the correct dir
-cd /home/container/${PROJECT}
+cd /home/container/bdsx
 
-# Run npm i
-echo ":/home/container/${PROJECT}$ npm i"
-npm i
-
-# Run BDS i
-echo ":/home/container/${PROJECT}$ npx -c 'bdsx i -y'"
-npx -c 'bdsx i -y'
-
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container/${PROJECT}$ ${MODIFIED_STARTUP}"
-
-# Run the Server
-${MODIFIED_STARTUP}
+# Run bdsx
+bash bdsx.sh
